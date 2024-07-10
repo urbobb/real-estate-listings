42388d8ecdbb0bff2fe5973b5d8e210a0725f20c
Bangerr <devilism09@gmail.com>
2024-07-10 18:50:11 +0200

    updated navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,23 @@
-const Navbar = () => {
+import useMediaQuery from "../hooks/userMediaQuery";
+
+type Props = {
+  isTopOfPage?: boolean;
+};
+
+const Navbar = ({ isTopOfPage }: Props) => {
+  const navbarBackground = !isTopOfPage ? "" : "bg-slate-800";
+  const flexBetween = `flex justify-between items-center`;
+
   const styleLinks = `flex items-center justify-center hover:border-2 border-slate-700 
   rounded-lg px-5 mx-auto h-10 hover:bg-black hover:text-white hover:underline`;
-  const logoHover = `duration-400 hover:cursor-crosshair 
-          hover:bg-[#5ADDEF] hover:text-white hover:border-1 border-black rounded-lg`;
+  const logoHover = `duration-400 hover:cursor-crosshair hover:bg-[#5ADDEF] hover:text-white 
+  hover:border-1 border-black rounded-lg`;
 
   return (
-    <nav className="navbar flex justify-between text-slate-500 my-5 mx-10">
+    <nav
+      className={`${navbarBackground} ${flexBetween} navbar text-slate-500 my-5 mx-10`}>
       <div
-        className={`${logoHover} logo flex items-center justify-center px-5 text-lg
-                  `}>
+        className={`${flexBetween} ${logoHover} logo h-10 px-5 text-lg font-extrabold tracking-widest`}>
         <a href="/">Real Estate</a>
       </div>
 
