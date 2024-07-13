4c55bdff2ba389d597c3884e3254f1f60abb3377
Bangerr <devilism09@gmail.com>
2024-07-14 00:24:28 +0200

    added ListingsCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Spline from "@splinetool/react-spline/next";
 import Image from "next/image";
-import JPMorgan from "./assets/JPMorganChase_Logo.png";
-import XLogo from "./assets/X_Logo.png";
-import CoinbaseLogo from "./assets/Coinbase_Logo.png";
-import MetaLogo from "./assets/Meta_Logo.png";
+import JPMorgan from "./assets/sponsors/JPMorganChase_Logo.png";
+import XLogo from "./assets/sponsors/X_Logo.png";
+import CoinbaseLogo from "./assets/sponsors/Coinbase_Logo.png";
+import MetaLogo from "./assets/sponsors/Meta_Logo.png";
+import ListingsCard from "./components/ListingsCard";
+import House1 from "./assets/listings/House1.png";
 
 export default function Home() {
   return (
@@ -46,6 +48,36 @@ export default function Home() {
           <div className="w-12">
             <Image src={XLogo} alt="X" />
           </div>
+        </div>
+      </div>
+
+      {/* LISTINGS */}
+      <div className="h-screen w-5/6 mx-auto pt-16">
+        <div className="mb-16">
+          <h1 className="text-2xl font-bold">Listings</h1>
+        </div>
+        <div className="grid grid-cols-4 gap-10">
+          <a href="/listings/1">
+            <ListingsCard listingsImage={House1} />
+          </a>
+          <a>
+            <ListingsCard listingsImage={House1} />
+          </a>
+          <a>
+            <ListingsCard listingsImage={House1} />
+          </a>
+          <a>
+            <ListingsCard listingsImage={House1} />
+          </a>
+          <a>
+            <ListingsCard listingsImage={House1} />
+          </a>
+          <a>
+            <ListingsCard listingsImage={House1} />
+          </a>
+          <a>
+            <ListingsCard listingsImage={House1} />
+          </a>
         </div>
       </div>
     </main>
