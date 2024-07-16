d4cd1ab70a4d3e363b0ff136f872a727c4394f18
Bangerr <devilism09@gmail.com>
2024-07-16 17:07:08 +0200

    created Link component

---
+++ app/components/Navbar/Links.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type Props = {
+  name: string;
+};
+
+const Links = ({ name }: Props) => {
+  const lowerCasePage = name.toLowerCase().replace(/ /g, "");
+
+  return (
+    <a
+      href={`/${lowerCasePage}`}
+      className="border-2 hover:border-slate-50 rounded-md p-1.5 
+          tracking-widest text-primary-100 transition duration-500 
+          hover:text-gray-20 hover:bg-blue-50">
+      {name}
+    </a>
+  );
+};
+
+export default Links;
