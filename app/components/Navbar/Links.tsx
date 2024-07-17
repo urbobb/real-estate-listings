49fbb7424a995030fde388b7fa5966f80fca470a
Bangerr <devilism09@gmail.com>
2024-07-17 19:38:12 +0200

    updated Links component for navbar

--- app/components/Navbar/Links.tsx
+++ app/components/Navbar/Links.tsx
@@ -12,7 +12,7 @@ const Links = ({ name }: Props) => {
       href={`/${lowerCasePage}`}
       className="border-2 hover:border-slate-50 rounded-md p-1.5 
           tracking-widest text-primary-100 transition duration-500 
-          hover:text-gray-20 hover:bg-blue-50">
+          hover:text-gray-20 hover:bg-blue-50 flex justify-center items-center">
       {name}
     </a>
   );
