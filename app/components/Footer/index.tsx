feee6c64cc9c8627373f50db546a4aeb5d581108
Bangerr <devilism09@gmail.com>
2024-07-21 23:06:01 +0200

    added Footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -3,7 +3,25 @@ import React from "react";
 type Props = {};
 
 const Footer = (props: Props) => {
-  return <div></div>;
+  return (
+    <footer id="footer" className="w-full py-8 ">
+      <div className="mx-auto md:w-11/12 w-9/12 flex justify-between z-40">
+        <div className="">
+          <p className="md:text-lg">© Designed by Bob</p>
+        </div>
+        <div className="w-20 flex justify-between hover:cursor-pointer">
+          <a href="https://www.linkedin.com/" target="_blank">
+            <i className="fa-brands fa-linkedin md:fa-2x fa-xl"></i>
+          </a>
+          <a
+            href="https://github.com/Bangerr/real-estate-listings"
+            target="_blank">
+            <i className="fa-brands fa-github md:fa-2x fa-xl"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
