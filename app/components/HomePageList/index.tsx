ff2c048164fe780d63c2d6e7d8c75a17019f1b1f
Bangerr <devilism09@gmail.com>
2024-07-24 19:55:53 +0200

    updated HomeListPage

--- app/components/HomePageList/index.tsx
+++ app/components/HomePageList/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import ListingsCard from "@/app/components/ListingsCard";
 import House1 from "@/app/assets/listings/House1.png";
 import House2 from "@/app/assets/listings/House2.jpg";
@@ -10,35 +10,102 @@ import House6 from "@/app/assets/listings/House6.jpg";
 type Props = {};
 
 const HomePageList = (props: Props) => {
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  function scrollLeft() {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollLeft -= 100;
+    }
+  }
+
+  function scrollRight() {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollLeft += 100;
+    }
+  }
   return (
-    <div className="md:min-h-max">
-      <div className="houseList md:h-5/6 md:gap-[50px] gap-2">
-        <div className="item1 col-start-1 col-end-1 row-span-2">
-          <ListingsCard listingsImage={House3} id={1} />
+    <div
+      ref={scrollContainerRef}
+      className="houseList w-full md:min-h-max overflow-x-auto overflow-y-hidden 
+              scroll-smooth ">
+      <div className=" md:h-5/6 gap-[5%] flex min-w-max">
+        <div className="item1">
+          <ListingsCard
+            listingsImage={House1}
+            location={"New york"}
+            area={76}
+            energyClass={"A"}
+            price={350}
+            id={1}
+          />
         </div>
-        <div className="item2 md:h-[200px] ">
-          <ListingsCard listingsImage={House2} id={2} />
+        <div className="item2">
+          <ListingsCard
+            listingsImage={House2}
+            location={"New york"}
+            area={76}
+            energyClass={"A"}
+            price={350}
+            id={1}
+          />
         </div>
-        <div className="item3 md:h-[200px]">
-          <ListingsCard listingsImage={House3} id={3} />
+        <div className="item3">
+          <ListingsCard
+            listingsImage={House3}
+            location={"New york"}
+            area={76}
+            energyClass={"A"}
+            price={350}
+            id={1}
+          />
         </div>
-        <div className="item4 row-start-3 row-end-3 md:h-[200px]">
-          <ListingsCard listingsImage={House4} id={4} />
+        <div className="item4">
+          <ListingsCard
+            listingsImage={House4}
+            location={"New york"}
+            area={76}
+            energyClass={"A"}
+            price={350}
+            id={1}
+          />
         </div>
-        <div className="item5 row-start-4 row-end-4 md:h-[200px]">
-          <ListingsCard listingsImage={House5} id={5} />
+        <div className="item5">
+          <ListingsCard
+            listingsImage={House5}
+            location={"New york"}
+            area={76}
+            energyClass={"A"}
+            price={350}
+            id={1}
+          />
         </div>
-        <div className="item6 col-start-2 col-end-3 row-span-2">
-          <ListingsCard listingsImage={House6} id={6} />
+        <div className="item6">
+          <ListingsCard
+            listingsImage={House6}
+            location={"New york"}
+            area={76}
+            energyClass={"A"}
+            price={350}
+            id={1}
+          />
         </div>
         <div className="item7">
-          <ListingsCard listingsImage={House1} id={7} />
+          <ListingsCard
+            listingsImage={House6}
+            location={"New york"}
+            area={76}
+            energyClass={"A"}
+            price={350}
+            id={1}
+          />
         </div>
         <div className="item8 flex justify-center items-center md:flex-row flex-col md:gap-5 gap-4 rounded-2xl border-2 border-solid border-stone-400">
-          <a className="" href="/listings">
-            Click for more
-          </a>
-          <i className="animate-slide fa-solid fa-arrow-right fa-2xl bg-black -ml-4"></i>
+          <div className="w-[19rem] sm:w-[22rem] md:w-[24rem] flex items-center justify-center">
+            <a className="" href="/listings">
+              Click for more
+            </a>
+            <i className="animate-slide fa-solid fa-arrow-right fa-2xl bg-black ml-2"></i>
+          </div>
         </div>
       </div>
     </div>
