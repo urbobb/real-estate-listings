4062e50f9958b89da539bbe12bea1b21d85f6dbd
Bangerr <devilism09@gmail.com>
2024-08-14 20:15:48 +0200

    save

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -72,5 +72,35 @@ export default async function Listings() {
 
   //console.log("Data: ", typeof handleAction());
 
-  return <SearchBarListings />;
+  return <div className="relative md:min-h-screen md:w-screen w-full mx-auto pt-20">
+  <div className="flex md:flex-row flex-col md:w-screen w-full md:min-h-screen  ">
+    {/* SEARCHBAR */}
+    <div
+      className="flex md:min-h-screen justify-center items-center
+      w-full md:w-[400px] bg-[#fcf9fd]">
+      {" "}
+      {/*bg-[#FEFCFF]*/}
+      <div
+        className="relative md:fixed md:top-0 flex flex-col justify-center items-center min-h-max 
+        md:w-auto w-11/12 mx-auto md:pt-28">
+        {/* TITLE */}
+        <div className="flex md:justify-start justify-center md:mb-5 mb-2 sm:mx-5 md:mt-0 mt-5">
+          <h1 className="md:text-[1.3rem] text-[1.5rem] md:font-extrabold font-semibold tracking-[10px]">
+            Search
+          </h1>
+        </div>
+
+        {/* FORM */}
+        <div className="md:w-auto w-full mx-auto mt-5">
+          
+        </div>
+      </div>
+    </div>
+
+    {/* Houses */}
+    <div className="w-full mx-auto flex  pt-20 ">
+      
+    </div>
+  </div>
+</div>;
 }
