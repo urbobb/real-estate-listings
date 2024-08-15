e996f78d28301e25e1248f429f986172dfaf3a1c
Bangerr <devilism09@gmail.com>
2024-08-15 17:27:47 +0200

    saving for later

--- app/components/SearchListings/index.tsx
+++ app/components/SearchListings/index.tsx
@@ -48,7 +48,7 @@ const SearchBarListings = ({}: Props) => {
     LAND: false,
   });
   const [priceState, setPriceState] = useState(510000);
-  const [dataReceivedDB, setDataReceivedDB] = useState([]);
+  const [dataReceivedDB, setDataReceivedDB] = useState<Listing[]>([]);
 
   const inputStyles = `w-full mb-2 min-h-max outline-0 text-[1.1em] 
   border-b-2 border-stone-400 focus:border-stone-200 transition duration-300
@@ -140,178 +140,172 @@ const SearchBarListings = ({}: Props) => {
   }
 
   return (
-    <div className="relative md:min-h-screen md:w-screen w-full mx-auto pt-20">
-      <div className="flex md:flex-row flex-col md:w-screen w-full md:min-h-screen  ">
-        {/* SEARCHBAR */}
-        <div
-          className="flex md:min-h-screen justify-center items-center
+    <div className="flex md:flex-row flex-col md:w-screen w-full md:min-h-screen  ">
+      {/* SEARCHBAR */}
+      <div
+        className="flex md:min-h-screen justify-center items-center
           w-full md:w-[400px] bg-[#fcf9fd]">
-          {" "}
-          {/*bg-[#FEFCFF]*/}
-          <div
-            className="relative md:fixed md:top-0 flex flex-col justify-center items-center min-h-max 
+        {" "}
+        {/*bg-[#FEFCFF]*/}
+        <div
+          className="relative md:fixed md:top-0 flex flex-col justify-center items-center min-h-max 
             md:w-auto w-11/12 mx-auto md:pt-28">
-            {/* TITLE */}
-            <div className="flex md:justify-start justify-center md:mb-5 mb-2 sm:mx-5 md:mt-0 mt-5">
-              <h1 className="md:text-[1.3rem] text-[1.5rem] md:font-extrabold font-semibold tracking-[10px]">
-                Search
-              </h1>
-            </div>
+          {/* TITLE */}
+          <div className="flex md:justify-start justify-center md:mb-5 mb-2 sm:mx-5 md:mt-0 mt-5">
+            <h1 className="md:text-[1.3rem] text-[1.5rem] md:font-extrabold font-semibold tracking-[10px]">
+              Search
+            </h1>
+          </div>
 
-            {/* FORM */}
-            <div className="md:w-auto w-full mx-auto mt-5">
-              <form
-                action=""
-                method="POST"
-                onSubmit={handleSubmit}
-                className="flex flex-col gap-5 md:w-[250px] w-9/12 mx-auto">
-                <div className="flex md:gap-10 gap-8 flex-col md:mb-10 mb-8">
-                  {/* TYPE */}
-                  <div className="flex flex-col items-start gap-2">
-                    <p>Type</p>
-                    <div className="w-full flex flex-row justify-around items-center ">
+          {/* FORM */}
+          <div className="md:w-auto w-full mx-auto mt-5">
+            <form
+              action=""
+              method="POST"
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-5 md:w-[250px] w-9/12 mx-auto">
+              <div className="flex md:gap-10 gap-8 flex-col md:mb-10 mb-8">
+                {/* TYPE */}
+                <div className="flex flex-col items-start gap-2">
+                  <p>Type</p>
+                  <div className="w-full flex flex-row justify-around items-center ">
+                    <SearchCheckbox
+                      name="listingType"
+                      labelName="Sale"
+                      checked={typeState.FOR_SALE}
+                      value={"FOR_SALE"}
+                      handleAction={() => handleTypeCheckboxChange("FOR_SALE")}
+                    />
+                    <SearchCheckbox
+                      name="listingType"
+                      labelName="Rent"
+                      checked={typeState.FOR_RENT}
+                      value={"FOR_RENT"}
+                      handleAction={() => handleTypeCheckboxChange("FOR_RENT")}
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col md:justify-center gap-2">
+                  <p>Category</p>
+                  <div className="flex flex-col gap-2">
+                    <div className="w-full flex flex-row justify-around gap-2">
                       <SearchCheckbox
-                        name="listingType"
-                        labelName="Sale"
-                        checked={typeState.FOR_SALE}
-                        value={"FOR_SALE"}
+                        name="propertyType"
+                        labelName="House"
+                        value={"HOUSE"}
+                        checked={catergoryState.HOUSE}
                         handleAction={() =>
-                          handleTypeCheckboxChange("FOR_SALE")
+                          handleCatergoryCheckboxChange("HOUSE")
                         }
                       />
+
                       <SearchCheckbox
-                        name="listingType"
-                        labelName="Rent"
-                        checked={typeState.FOR_RENT}
-                        value={"FOR_RENT"}
+                        name="PropertyType"
+                        labelName="Appartment"
+                        value={"APPARTMENT"}
+                        checked={catergoryState.APPARTMENT}
                         handleAction={() =>
-                          handleTypeCheckboxChange("FOR_RENT")
+                          handleCatergoryCheckboxChange("APPARTMENT")
+                        }
+                      />
+                    </div>
+                    <div className="w-full flex flex-row justify-around gap-2">
+                      <SearchCheckbox
+                        name="propertyType"
+                        labelName="Condo"
+                        value={"CONDO"}
+                        checked={catergoryState.CONDO}
+                        handleAction={() =>
+                          handleCatergoryCheckboxChange("CONDO")
+                        }
+                      />
+
+                      <SearchCheckbox
+                        name="propertyType"
+                        labelName="Land"
+                        value={"LAND"}
+                        checked={catergoryState.LAND}
+                        handleAction={() =>
+                          handleCatergoryCheckboxChange("LAND")
                         }
                       />
                     </div>
                   </div>
-                  <div className="flex flex-col md:justify-center gap-2">
-                    <p>Category</p>
-                    <div className="flex flex-col gap-2">
-                      <div className="w-full flex flex-row justify-around gap-2">
-                        <SearchCheckbox
-                          name="propertyType"
-                          labelName="House"
-                          value={"HOUSE"}
-                          checked={catergoryState.HOUSE}
-                          handleAction={() =>
-                            handleCatergoryCheckboxChange("HOUSE")
-                          }
-                        />
-
-                        <SearchCheckbox
-                          name="PropertyType"
-                          labelName="Appartment"
-                          value={"APPARTMENT"}
-                          checked={catergoryState.APPARTMENT}
-                          handleAction={() =>
-                            handleCatergoryCheckboxChange("APPARTMENT")
-                          }
-                        />
-                      </div>
-                      <div className="w-full flex flex-row justify-around gap-2">
-                        <SearchCheckbox
-                          name="propertyType"
-                          labelName="Condo"
-                          value={"CONDO"}
-                          checked={catergoryState.CONDO}
-                          handleAction={() =>
-                            handleCatergoryCheckboxChange("CONDO")
-                          }
-                        />
-
-                        <SearchCheckbox
-                          name="propertyType"
-                          labelName="Land"
-                          value={"LAND"}
-                          checked={catergoryState.LAND}
-                          handleAction={() =>
-                            handleCatergoryCheckboxChange("LAND")
-                          }
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  {/* PLACE */}
-                  <div className="flex flex-row gap-2">
-                    <p>City</p>
-                    <select className={`${inputStyles}`} name="city">
-                      <option value="All above">All above</option>
-                      <option value="Milano">Milano</option>
-                      <option value="Torino">Torino</option>
-                      <option value="Venezia">Venezia</option>
-                      <option value="Bologna">Bologna</option>
-                    </select>
-                  </div>
-                  {/* <div className="flex flex-row gap-2">
+                </div>
+                {/* PLACE */}
+                <div className="flex flex-row gap-2">
+                  <p>City</p>
+                  <select className={`${inputStyles}`} name="city">
+                    <option value="All above">All above</option>
+                    <option value="Milano">Milano</option>
+                    <option value="Torino">Torino</option>
+                    <option value="Venezia">Venezia</option>
+                    <option value="Bologna">Bologna</option>
+                  </select>
+                </div>
+                {/* <div className="flex flex-row gap-2">
                     <p>Zone</p>
                     <input type="text" className={`${inputStyles}`} />
                   </div> */}
 
-                  <div className="flex flex-col md:gap-10 gap-8">
-                    <div className="flex flex-col gap-2">
-                      <label htmlFor="">Price</label>
-                      <input
-                        name="price"
-                        type="range"
-                        min="10000"
-                        max="1000000"
-                        step={10000}
-                        className="slider w-full mb-2 min-h-max"
-                        onChange={(e) => {
-                          handlePriceChange(e.target.value);
-                        }}
-                      />
-                      <p>{priceState.toLocaleString()} €</p>
-                    </div>
+                <div className="flex flex-col md:gap-10 gap-8">
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="">Price</label>
+                    <input
+                      name="price"
+                      type="range"
+                      min="10000"
+                      max="1000000"
+                      step={10000}
+                      className="slider w-full mb-2 min-h-max"
+                      onChange={(e) => {
+                        handlePriceChange(e.target.value);
+                      }}
+                    />
+                    <p>{priceState.toLocaleString()} €</p>
+                  </div>
 
-                    <div className="flex flex-col gap-2">
-                      <label htmlFor="">
-                        Area in m<sup>2</sup>
-                      </label>
-                      <select name="area" className={`${inputStyles}`}>
-                        <option value="All above">All above</option>
-                        <option value="60">60m&#178;</option>
-                        <option value="70">70m&#178;</option>
-                        <option value="80">80m&#178;</option>
-                        <option value="90">90m&#178;</option>
-                        <option value="100">100m&#178;</option>
-                      </select>
-                    </div>
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="">
+                      Area in m<sup>2</sup>
+                    </label>
+                    <select name="area" className={`${inputStyles}`}>
+                      <option value="All above">All above</option>
+                      <option value="60">60m&#178;</option>
+                      <option value="70">70m&#178;</option>
+                      <option value="80">80m&#178;</option>
+                      <option value="90">90m&#178;</option>
+                      <option value="100">100m&#178;</option>
+                    </select>
                   </div>
                 </div>
-                <button
-                  className="-mt-5 rounded-lg border border-black px-8 py-3 transition 
+              </div>
+              <button
+                className="-mt-5 rounded-lg border border-black px-8 py-3 transition 
               duration-500 hover:text-white hover:scale-90 hover:bg-black">
-                  Search
-                </button>
-              </form>
-            </div>
+                Search
+              </button>
+            </form>
           </div>
         </div>
+      </div>
 
-        {/* Houses */}
-        <div className="w-full mx-auto flex  pt-20 ">
-          <div className="flex justify-center md:items-start items-center flex-row flex-wrap gap-y-5 gap-x-28">
-            {dataReceivedDB.map((house) => (
-              <div
-                className={`flex-[0_0_calc(20%_-_1rem)] box-border`}
-                key={house.id}>
-                <ListingsCard
-                  location={house.location}
-                  area={house.area}
-                  bedrooms={house.bedrooms}
-                  price={house.price}
-                  id={house.id}
-                />
-              </div>
-            ))}
-          </div>
+      {/* Houses */}
+      <div className="w-full mx-auto flex  pt-20 ">
+        <div className="flex justify-center md:items-start items-center flex-row flex-wrap gap-y-5 gap-x-28">
+          {dataReceivedDB.map((house) => (
+            <div
+              className={`flex-[0_0_calc(20%_-_1rem)] box-border`}
+              key={house.id}>
+              <ListingsCard
+                location={house.location}
+                area={house.area}
+                bedrooms={house.bedrooms}
+                price={house.price}
+                id={house.id}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
