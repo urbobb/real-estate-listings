344acc8382899a3e70616409ddeeac0c64f9f745
Bangerr <devilism09@gmail.com>
2024-08-18 00:15:47 +0200

    updated footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -7,7 +7,7 @@ const Footer = (props: Props) => {
     <footer id="footer" className="bg-[#ffffff] w-full py-8 mb-10 text-sm">
       <div className="mx-auto md:w-5/6 w-9/12 flex justify-between z-40 border-t pt-5">
         <div className="">
-          <p className="">© 2024 Home Finder. All rights reserved.</p>
+          <p className="">© 2024 Home Finder. Designed by Sukh.</p>
         </div>
         <div className="flex justify-between gap-8 hover:cursor-pointer">
           <a href="/privacypolicy" target="_blank">
