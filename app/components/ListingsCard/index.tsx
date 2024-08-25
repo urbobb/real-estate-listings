8a2cbaf272e58ab049ed2a242812419be7b99b71
Bangerr <devilism09@gmail.com>
2024-08-25 16:29:31 +0200

    updated ListingCard: images

--- app/components/ListingsCard/index.tsx
+++ app/components/ListingsCard/index.tsx
@@ -1,8 +1,10 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import House1 from "@/app/assets/listings/House1.jpg";
 
 type Props = {
+  image: string;
   location: string;
   area: number;
   bedrooms: number;
@@ -10,15 +12,22 @@ type Props = {
   id: number;
 };
 
-const ListingsCard = ({ location, area, bedrooms, price, id }: Props) => {
+const ListingsCard = ({
+  image,
+  location,
+  area,
+  bedrooms,
+  price,
+  id,
+}: Props) => {
   return (
     <div className=" bg-[#ffffff] rounded overflow-hidden mb-5 drop-shadow w-[19rem] sm:w-[22rem] md:w-[24rem]">
       <a href={`/listings/${id}`} target="_blank" className="w-full">
-        <div className="md:w-full h-[200px]  overflow-hidden ">
-          <Image
+        <div className="md:w-full h-[200px]  overflow-hidden">
+          <img
             className="md:h-full md:w-full rounded hover:scale-105 duration-500 ease-out delay-100 hover:cursor-pointer "
-            src={House1}
-            alt={House1.toString()}
+            src={`https://homefinderr-images.s3.eu-north-1.amazonaws.com/${image}`}
+            alt={"House image"}
           />
         </div>
         <div className="min-h-fit mt-5 ml-2 w-full">
