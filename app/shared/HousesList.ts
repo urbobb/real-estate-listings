4f2ff869d30a8642a7e4aac13103a1386f141438
Bangerr <devilism09@gmail.com>
2024-08-25 19:42:56 +0200

    commit zi kanne

--- app/shared/HousesList.ts
+++ app/shared/HousesList.ts
@@ -226,7 +226,7 @@ export const houses = [
       "Welcome to the Mod on Trenton! A contemporary new construction home on the cusp of vibrant neighborhoods Edgewood and Kirkwood. It features 5 bedrooms and 3.5 bathrooms, 2- car garage, a private main-level deck, and a stunning rooftop balcony featuring a cozy fire pit. This home boasts exterior columns and a large end-to-end porch that add charm and character. As you walk inside, you’re greeted with hardwood oak floors throughout, 9-foot ceilings, granite kitchen countertops, a spacious island for food prep or casual dining, and an oversized pantry. Step into your dining room and family room near the kitchen, creating a seamless flow perfect for dining and entertaining. On the main floor, will find a guest room with an ensuite bathroom, half bathroom, and spacious backyard. Retreat to your first-floor deck off the dining room, your front porch or journey to the second-level balcony to relax. The owner’s retreat features private access to the rooftop balcony, a large master bathroom with spacious his and her closets, double vanity sinks, a separate walk-in shower with double jets that complement the home’s luxurious essence. Visit your nearby East Lake Golf Club, Edgewood Retail District, East Atlanta Village, Trolley Trail, and various dining options in this lively community. This home is convenient to I-20, 75/85, 285, the Beltline, Downtown Atlanta, Emory Hospital, Emory University, and other great amenities Atlanta has to offer. 3D renderings are for illustrative purposes only. Available in August/September 2024.",
     energyClass: "A",
     price: 300000,
-    yearBuilt: 2015,
+    yearBuilt: 20156,
     id: 10,
   },
 ];
