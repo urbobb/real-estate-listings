ab0413ba5c3b997877f3fc33ce5b417c3f6b98a9
Bangerr <devilism09@gmail.com>
2024-09-03 21:02:49 +0200

    added shadcn carousel for ImageGallery

--- app/components/ImageGallery/index.tsx
+++ app/components/ImageGallery/index.tsx
@@ -1,12 +1,27 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  type CarouselApi,
+} from "@/components/ui/carousel";
 import { useEffect, useRef, useState } from "react";
 
 interface Images {
   images: string[];
+  clicked: boolean;
 }
 
-export default function ImageGallery({ images }: Images) {
+export default function ImageGallery({ images, clicked }: Images) {
   const [bigImage, setBigImage] = useState<string>("");
   const [bigImageClicked, setBigImageClicked] = useState<boolean>(false);
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  let thumbnailImages: string[] = [];
+  thumbnailImages = images;
   const listingType = {
     sale: "SALE",
     rent: "RENT",
@@ -18,12 +33,27 @@ export default function ImageGallery({ images }: Images) {
     : " h-full w-full";
 
   useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  useEffect(() => {
     if (images.length > 0) {
       setBigImage(images[0]);
     }
   }, [images]);
 
   const handleBigImageClick = (image: string) => {
+    console.log(image);
+
     setBigImage(image);
   };
 
@@ -31,38 +61,59 @@ export default function ImageGallery({ images }: Images) {
     setBigImageClicked((prevState) => !prevState);
   };
 
-  function scrollLeft() {
-    if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollLeft -= 100;
-    }
-  }
-
-  function scrollRight() {
-    if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollLeft += 100;
-    }
-  }
-
   return (
     <div className="flex gap-4 flex-col lg:items-center md:w-full w-screen">
       <div className="grid gap-4 lg:grid-cols-2">
         <div
           className={`relative overflow-hidden md:rounded-lg bg-gray-100 lg:col-span-5 
-        lg:h-[500px] h-[250px] lg:w-[700px] w-full mx-auto`}>
-          <img
-            src={`https://homefinderr-images.s3.eu-north-1.amazonaws.com/${bigImage}`}
-            alt="image"
-            width={500}
-            height={500}
-            className={`${onClickImageStyle} object-cover object-center`}
-          />
-          <span className="absolute left- top-0 rounded-br-lg bg-red-500 px-3 py-1.5 uppercase tracking-wider text-white">
-            {listingType.sale}
-          </span>
+         w-full mx-auto lg:h-[500px] h-[250px] lg:w-[700px] ${
+           clicked ? "scale-up-image" : ""
+         }`}>
+          <Carousel setApi={setApi}>
+            <CarouselContent className="w-full ">
+              {thumbnailImages.map((image: any, idx: any) => (
+                <CarouselItem
+                  key={idx}
+                  className="overflow-hidden rounded-lg bg-gray-100 md:w-[100px] w-[70px]">
+                  <img
+                    src={`https://homefinderr-images.s3.eu-north-1.amazonaws.com/${image}`}
+                    alt="image"
+                    className={`${onClickImageStyle} object-cover object-center aspect-square `}
+                  />
+                  <span className="absolute left- top-0 rounded-br-lg bg-red-500 px-3 py-1.5 uppercase tracking-wider text-white">
+                    {listingType.sale}
+                  </span>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         </div>
       </div>
 
       <div
+        className="fullPageScroll flex flex-row gap-4 lg:order-none md:mx-0 mx-2
+            lg:w-[700px] overflow-x-auto overflow-y-hidden scroll-smooth">
+        <div className="flex flex-row justify-center min-w-max lg:gap-5 gap-2">
+          {images.map((image: any, index: any) => (
+            <div
+              key={index}
+              className="overflow-hidden rounded-lg bg-gray-100 md:w-[100px] w-[70px] hover:border-2 hover:border-blue-500 hover:cursor-pointer">
+              <img
+                src={`https://homefinderr-images.s3.eu-north-1.amazonaws.com/${image}`}
+                alt="image"
+                width={200}
+                height={200}
+                className="h-full w-full object-cover object-center cursor-pointer"
+                onClick={() => api && api.scrollTo(index)}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* <div
         ref={scrollContainerRef}
         className=" fullPageScroll flex flex-row gap-4 lg:order-none md:mx-0 mx-2
             lg:w-[700px] overflow-x-auto overflow-y-hidden scroll-smooth ">
@@ -70,19 +121,19 @@ export default function ImageGallery({ images }: Images) {
           {images.map((image: any, idx: any) => (
             <div
               key={idx}
-              className="overflow-hidden rounded-lg bg-gray-100 md:w-[100px] w-[70px]">
+              className="overflow-hidden rounded-lg bg-gray-100 md:w-[100px] w-[70px] hover:border-2 hover:border-blue-500">
               <img
                 src={`https://homefinderr-images.s3.eu-north-1.amazonaws.com/${image}`}
                 alt="image"
                 width={200}
                 height={200}
                 className="h-full w-full object-cover object-center cursor-pointer"
-                onClick={() => handleBigImageClick(image)}
+                onClick={() => handleBigImageClick(idx)}
               />
             </div>
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
--- app/listings/[id]/page.tsx
+++ app/listings/[id]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { houses } from "@/app/shared/HousesList";
 import ContactInfo from "@/app/components/ContactInfo";
 import ImageGallery from "@/app/components/ImageGallery";
 import balcony from "@/app/assets/balcony.png";
@@ -41,6 +40,7 @@ export default function List({ params }: { params: { id: string } }) {
   const pathname = usePathname();
   const [dataReceivedDB, setDataReceivedDB] = useState<Listing | null>(null);
   const [imagesUrl, setImagesUrl] = useState<string[]>([]);
+  const [isGalleryClicked, setIsGalleryClicked] = useState<boolean>(false);
 
   const detailsStyle = `flex flex-col`;
   const detailsContentStyle = `text-sm ml-7`;
@@ -75,154 +75,162 @@ export default function List({ params }: { params: { id: string } }) {
     fetchListing();
   }, [pathname]);
 
+  const handleGalleryClicked = () => {};
+
   return (
     <div className="w-full md:p-24 md:pt-24 pt-[70px] my-auto">
-      <div className="flex flex-col gap-8 md:h-full md:w-9/12 w-full mx-auto md:py-10 pt-5 pb-10 drop-shadow-sm ">
+      <div className="flex flex-col gap-8 md:h-full md:w-9/12 w-full mx-auto md:py-10 pt-5 pb-10 drop-shadow-sm">
         {/* IMAGES */}
-        <div className="grid gap-8 md:grid-cols-1 w-full">
-          <ImageGallery images={imagesUrl} />
+        <div
+          className="grid gap-8 md:grid-cols-1 w-full"
+          onClick={handleGalleryClicked}>
+          <ImageGallery images={imagesUrl} clicked={isGalleryClicked} />
         </div>
-        <div className="md:w-full w-11/12 mx-auto">
-          <div className="description  flex lg:flex-row flex-col md:gap-16 gap-5 p-5 justify-between ">
-            <div className="flex w-full">
-              <div className="w-full flex md:flex-row flex-col justify-between">
-                <div className="w-full flex flex-col gap-3">
-                  <div className="flex flex-col gap-5">
-                    <div className="flex md:flex-row flex-col gap-5 justify-between">
-                      <div className="flex flex-col gap-5">
-                        <h1 className="font-bold text-xl">
-                          {dataReceivedDB?.title}
-                        </h1>
-                        <div className="flex flex-row gap-5">
-                          <p>
-                            <i className="fa-solid fa-bed"></i>{" "}
-                            {dataReceivedDB?.bedrooms} Beds
-                          </p>
-                          <p>
-                            <i className="fa-solid fa-bath"></i>{" "}
-                            {dataReceivedDB?.bathrooms} Baths
-                          </p>
-                          <p>
-                            <i className="fa-solid fa-ruler"></i>{" "}
-                            {dataReceivedDB?.area}
-                            m&#178;
-                          </p>
-                        </div>
-                      </div>
-
-                      {/* PRICE AND LOCATION */}
-                      <div className="">
-                        <p className="font-bold text-[1.2rem] mb-2">
-                          {dataReceivedDB?.price === null
-                            ? "Preis auf Anfrage"
-                            : dataReceivedDB?.price?.toLocaleString()}{" "}
-                          €
-                        </p>
-                        <p>Location: {dataReceivedDB?.location}</p>
-                      </div>
-                    </div>
-                    {/* DETAILS */}
-                    <div className="flex-row border-t border-b w-full py-2">
-                      <h1 className="mb-3 font-semibold w-full border bg-gray-200 rounded p-2">
-                        <i className="fa-solid fa-house"></i> Property
-                        Information
-                      </h1>
-                      <div className="grid grid-cols-2 md:gap-y-2 gap-y-4">
-                        <ListingDetailItem
-                          title={"Property Type"}
-                          content={dataReceivedDB?.propertyType}
-                          icon={`fa-solid fa-building mr-2`}
-                        />
-                        <ListingDetailItem
-                          title={"Contract"}
-                          content={dataReceivedDB?.listingType}
-                          icon={`fa-solid fa-file-signature mr-2`}
-                        />
-                        <ListingDetailItem
-                          title={"Floors"}
-                          content={dataReceivedDB?.floors}
-                          icon={`fa-solid fa-stairs mr-2`}
-                        />
-                        <ListingDetailItem
-                          title={"Building Floors"}
-                          content={dataReceivedDB?.buildingFloors}
-                          icon={`fa-solid fa-building mr-2`}
-                        />
-                        <ListingDetailItem
-                          title={"Elevator"}
-                          content={dataReceivedDB?.elevator ? "Yes" : "No"}
-                          icon={`fa-solid fa-elevator mr-2`}
-                        />
-                        <ListingDetailItem
-                          title={"Area"}
-                          content={`${dataReceivedDB?.area}m²`}
-                          icon={`fa-solid fa-elevator mr-2`}
-                        />
-                        <ListingDetailItem
-                          title={"Bedrooms"}
-                          content={dataReceivedDB?.bedrooms}
-                          icon={`fa-solid fa-bed mr-2`}
-                        />
-                        <ListingDetailItem
-                          title={"Bathrooms"}
-                          content={dataReceivedDB?.bathrooms}
-                          icon={`fa-solid fa-bath mr-2`}
-                        />
-                        <ListingDetailItem
-                          title={"Furnished"}
-                          content={dataReceivedDB?.furnished}
-                          icon={`fa-solid fa-couch mr-2`}
-                        />
-
-                        <div className={`${detailsStyle}`}>
-                          <div className=" flex flex-row gap-2">
-                            <Image
-                              src={balcony}
-                              alt="balconyImage"
-                              height={18}
-                            />
-                            <h2 className="font-semibold md:text-base text-[0.9rem]">
-                              Balcony
-                            </h2>
+        {!isGalleryClicked ? (
+          <div className="md:w-full w-11/12 mx-auto">
+            <div className="description  flex lg:flex-row flex-col md:gap-16 gap-5 p-5 justify-between">
+              <div className="flex w-full">
+                <div className="w-full flex md:flex-row flex-col justify-between">
+                  <div className="w-full flex flex-col gap-3">
+                    <div className="flex flex-col gap-5">
+                      <div className="flex md:flex-row flex-col gap-5 justify-between">
+                        <div className="flex flex-col gap-5">
+                          <h1 className="font-bold text-xl">
+                            {dataReceivedDB?.title}
+                          </h1>
+                          <div className="flex flex-row gap-5">
+                            <p>
+                              <i className="fa-solid fa-bed"></i>{" "}
+                              {dataReceivedDB?.bedrooms} Beds
+                            </p>
+                            <p>
+                              <i className="fa-solid fa-bath"></i>{" "}
+                              {dataReceivedDB?.bathrooms} Baths
+                            </p>
+                            <p>
+                              <i className="fa-solid fa-ruler"></i>{" "}
+                              {dataReceivedDB?.area}
+                              m&#178;
+                            </p>
                           </div>
-                          <p className={`${detailsContentStyle}`}>
-                            {dataReceivedDB?.balcony ? "Yes" : "No"}
-                          </p>
                         </div>
 
-                        <ListingDetailItem
-                          title={"Garage"}
-                          content={dataReceivedDB?.garage}
-                          icon={`fa-solid fa-car mr-2`}
-                        />
-                        <ListingDetailItem
-                          title={"Heating"}
-                          content={dataReceivedDB?.heating}
-                          icon={`fa-solid fa-fire mr-2`}
-                        />
-                        <ListingDetailItem
-                          title={"Energy Class"}
-                          content={dataReceivedDB?.energyclass}
-                          icon={`fa-solid fa-fire mr-2`}
-                        />
+                        {/* PRICE AND LOCATION */}
+                        <div className="">
+                          <p className="font-bold text-[1.2rem] mb-2">
+                            {dataReceivedDB?.price === null
+                              ? "Preis auf Anfrage"
+                              : dataReceivedDB?.price?.toLocaleString()}{" "}
+                            €
+                          </p>
+                          <p>Location: {dataReceivedDB?.location}</p>
+                        </div>
+                      </div>
+                      {/* DETAILS */}
+                      <div className="flex-row border-t border-b w-full py-2">
+                        <h1 className="mb-3 font-semibold w-full border bg-gray-200 rounded p-2">
+                          <i className="fa-solid fa-house"></i> Property
+                          Information
+                        </h1>
+                        <div className="grid grid-cols-2 md:gap-y-2 gap-y-4">
+                          <ListingDetailItem
+                            title={"Property Type"}
+                            content={dataReceivedDB?.propertyType}
+                            icon={`fa-solid fa-building mr-2`}
+                          />
+                          <ListingDetailItem
+                            title={"Contract"}
+                            content={dataReceivedDB?.listingType}
+                            icon={`fa-solid fa-file-signature mr-2`}
+                          />
+                          <ListingDetailItem
+                            title={"Floors"}
+                            content={dataReceivedDB?.floors}
+                            icon={`fa-solid fa-stairs mr-2`}
+                          />
+                          <ListingDetailItem
+                            title={"Building Floors"}
+                            content={dataReceivedDB?.buildingFloors}
+                            icon={`fa-solid fa-building mr-2`}
+                          />
+                          <ListingDetailItem
+                            title={"Elevator"}
+                            content={dataReceivedDB?.elevator ? "Yes" : "No"}
+                            icon={`fa-solid fa-elevator mr-2`}
+                          />
+                          <ListingDetailItem
+                            title={"Area"}
+                            content={`${dataReceivedDB?.area}m²`}
+                            icon={`fa-solid fa-elevator mr-2`}
+                          />
+                          <ListingDetailItem
+                            title={"Bedrooms"}
+                            content={dataReceivedDB?.bedrooms}
+                            icon={`fa-solid fa-bed mr-2`}
+                          />
+                          <ListingDetailItem
+                            title={"Bathrooms"}
+                            content={dataReceivedDB?.bathrooms}
+                            icon={`fa-solid fa-bath mr-2`}
+                          />
+                          <ListingDetailItem
+                            title={"Furnished"}
+                            content={dataReceivedDB?.furnished}
+                            icon={`fa-solid fa-couch mr-2`}
+                          />
+
+                          <div className={`${detailsStyle}`}>
+                            <div className=" flex flex-row gap-2">
+                              <Image
+                                src={balcony}
+                                alt="balconyImage"
+                                height={18}
+                              />
+                              <h2 className="font-semibold md:text-base text-[0.9rem]">
+                                Balcony
+                              </h2>
+                            </div>
+                            <p className={`${detailsContentStyle}`}>
+                              {dataReceivedDB?.balcony ? "Yes" : "No"}
+                            </p>
+                          </div>
+
+                          <ListingDetailItem
+                            title={"Garage"}
+                            content={dataReceivedDB?.garage}
+                            icon={`fa-solid fa-car mr-2`}
+                          />
+                          <ListingDetailItem
+                            title={"Heating"}
+                            content={dataReceivedDB?.heating}
+                            icon={`fa-solid fa-fire mr-2`}
+                          />
+                          <ListingDetailItem
+                            title={"Energy Class"}
+                            content={dataReceivedDB?.energyclass}
+                            icon={`fa-solid fa-fire mr-2`}
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
 
-                  <div className="flex flex-col gap-5 mt-10 basis-[25%]">
-                    <h1>Description:</h1>
-                    <p className="text-justify">
-                      {dataReceivedDB?.description}
-                    </p>
+                    <div className="flex flex-col gap-5 mt-10 basis-[25%]">
+                      <h1>Description:</h1>
+                      <p className="text-justify">
+                        {dataReceivedDB?.description}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="relative flex justify-center h-full border rounded-lg md:mt-0 mt-10">
-              <ContactInfo />
+              <div className="relative flex justify-center h-full border rounded-lg md:mt-0 mt-10">
+                <ContactInfo />
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
