3e32bccb595d1b51444ddde69d8984a21a586b4a
Bangerr <devilism09@gmail.com>
2024-09-05 23:19:31 +0200

    added Admin Page button

--- app/admin/addListing/page.tsx
+++ app/admin/addListing/page.tsx
@@ -2,6 +2,7 @@ import { auth, signOut } from "@/auth";
 import React from "react";
 import FormCreateListing from "./FormCreateListing";
 import { redirect } from "next/navigation";
+import HomePageButton from "./HomePageButton";
 
 type Props = {};
 
@@ -30,9 +31,12 @@ export default async function AdminPage({}: Props) {
   }
   return (
     <div className="min-h-screen">
-      <div className="flex flex-col mt-28">
-        <div className="lg:w-1/2 w-full mx-auto flex justify-end items-center">
-          um auszuloggen {"-->"} <SignOutButton />
+      <div className="flex flex-col mt-28 gap-5">
+        <div className="lg:w-1/2 w-full mx-auto flex justify-between items-center">
+          <HomePageButton />
+          <div className="flex flex-row gap-3 items-center">
+            um auszuloggen {"-->"} <SignOutButton />
+          </div>
         </div>
         <FormCreateListing />
       </div>
